aedd5aff2d98e2f153d2aeaa2dfd391f7275b572
SmrutiPatil <patilsmrutim248@gmail.com>
2024-04-07 05:10:59 +0530

    added cards to booklet

--- frontend/src/components/Booklet.jsx
+++ frontend/src/components/Booklet.jsx
@@ -18,7 +18,7 @@ const Page = React.forwardRef((props, ref) => {
     <div className="page" ref={ref}>
       <div className="page-content">
         <h2 className="page-header">Page header - {props.number}</h2>
-        <div className="page-image"></div>
+        <div className="page-image"> <CardDef animals={props.animals} /></div>
         <div className="page-text">{props.children}</div>
         <div className="page-footer">{props.number + 1}</div>
       </div>
@@ -81,8 +81,8 @@ class DemoBook extends React.Component {
           ref={(el) => (this.flipBook = el)}
         >
           <PageCover>THE ENCYCLOPEDIA</PageCover>
-          <Page number={1}>Lorem ipsum...</Page>
-          <Page number={2}>Lorem ipsum...</Page>
+          <Page number={1} animal={"lion"}>Lorem ipsum...</Page>
+          <Page number={2} animal={"hibiscus"}>Lorem ipsum...</Page>
           <PageCover>THE END</PageCover>
         </HTMLFlipBook>
       </div>
